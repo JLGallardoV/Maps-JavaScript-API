e2b6f9239ab53e21d743ecac1b86ed34fbc8a5e0
JLGallardoV <Gallardo.vaca@gmail.com>
2020-01-21 12:37:52 -0600

    MAPS JS - PROCESO
    - Funcion inicializar mapa completada:
    	- Se inicializo en irapuato y se puso un marker en la ubicacion especificada

--- maps.js
+++ maps.js
@@ -1,10 +1,16 @@
 //FUNCION PARA INICIALIZAR EL MAPA
 function initMap(){
+  miUbicacion = {lat: 20.6786652, lng: -101.35449640000002} //objeto para especificar mi ubicacion
   //manipulacion del DOM para mostrar el mapa
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: {lat: 20.6786652, lng: -101.35449640000002} //COORDENADAS DE IRAPUATO
+    zoom: 12, //nivel de zoom
+    center: miUbicacion
   });
 
+  var marker = new google.maps.Marker({
+     position: miUbicacion, //objeto con mis coordenadas
+     map: map, //el mapa en uso
+     title: 'Aqui estoy' //msj marker
+   });
 
 }
