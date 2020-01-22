6974a3f9374d8e4821c6a4838408413f2cfaa24d
JLGallardoV <Gallardo.vaca@gmail.com>
2020-01-22 11:15:33 -0600

    MAPS JS - PROCESO
    - En proceso para pintar la ruta de un punto x a un punto y

--- maps.js
+++ maps.js
@@ -3,11 +3,16 @@ var map; //representa el mapa
 var arregloMarkers = []; //almacena los markers de esta practica
 var marker;
 var id = 0; //representa un id autoincrementable para cada marker
+var inputOrigen = document.getElementById('idOrigen');//representa la ruta x
+var inputDestino = document.getElementById('idDestino');//representa la ruta y
+
+
 
 //FUNCION PARA INICIALIZAR EL MAPA
 function initMap() {
-  var inputOrigen = document.getElementById('idOrigen');//representa la ruta x
-  var inputDestino = document.getElementById('idDestino');//representa la ruta y
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
   miUbicacion = {
     lat: 20.6786652,
     lng: -101.35449640000002
@@ -28,6 +33,19 @@ function initMap() {
 
   //agregando marker cuando se inicia el mapa (invocando funcion)
   addMarker(miUbicacion, map);
+  //autocompletando inputs (invocando funcion)
+  autocompletarInputs();
+
+  directionsDisplay.setMap(map);
+
+  //FUNCION PARA DETECTAR Y MANIPULAR LOS CAMBIOS HECHOS POR LA FUNCION INVOCADA EN LOS INPUTS
+  var onChangeHandler = function() {
+    console.log("hola");
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  }
+  //invocamos la funcion onChangeHandler al momento de detectar un cambio en los inputs
+  inputOrigen.addEventListener('change', onChangeHandler);
+  inputDestino.addEventListener('change', onChangeHandler);
 
   //esta funcionalidad ejecuta una accion  una vez el usuario clickea el mapa
   google.maps.event.addListener(map, 'click', function(event) {
@@ -38,18 +56,9 @@ function initMap() {
       arregloMarkers[arregloMarkers.length-1].setMap(null); //ocultamos del mapa el ultimo marker generado
       arregloMarkers.pop(); //extraemos mediante un pop el ultimo elemento del arreglo
     })
-
   });
 
 
-  //FUNCION PARA AUTOCOMPLETAR EL INPUT DE ORIGEN
-  var autocompleteOrigen = new google.maps.places.Autocomplete(inputOrigen);
-  autocompleteOrigen.bindTo('bounds', map);//restringe los resultados del input
-  autocompleteOrigen.setFields(['address_components', 'geometry', 'icon', 'name']);//establece los campos que se van a ver en los detalles del lugar
-  //FUNCION PARA AUTOCOMPLETAR EL INPUT DE DESTINO
-  var autocompleteDestino = new google.maps.places.Autocomplete(inputDestino);
-  autocompleteDestino.bindTo('bounds', map);//restringe los resultados del input
-  autocompleteDestino.setFields(['address_components', 'geometry', 'icon', 'name']);//establece los campos que se van a ver en los detalles del lugar
 
 }
 
@@ -66,3 +75,39 @@ function addMarker(location, map) {
   arregloMarkers.push(marker); //agregamos los markers a un arreglo para poder manipularlos posteriormente
   console.log("longitud del arreglo contenedor de markers: ", arregloMarkers.length);
 }
+
+
+
+//FUNCION PARA AUTOCOMPLETAR LOS INPUTS
+function autocompletarInputs(){
+  //origen
+  var autocompleteOrigen = new google.maps.places.Autocomplete(inputOrigen);
+  autocompleteOrigen.bindTo('bounds', map); //restringe los resultados, los hace mas locales
+  autocompleteOrigen.setFields(['address_components', 'geometry', 'icon', 'name']);//establece los campos que se van a ver en los detalles del lugar
+
+  //destino
+  var autocompleteDestino = new google.maps.places.Autocomplete(inputDestino);
+  autocompleteDestino.bindTo('bounds', map);//restringe los resultados, los hace mas locales
+  autocompleteDestino.setFields(['address_components', 'geometry', 'icon', 'name']);//establece los campos que se van a ver en los detalles del lugar
+}
+
+
+//FUNCION PARA TRAZAR UNA RUTA SEGUN UN PUNTO x Y UN PUNTO y
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  console.log("entro a la funcion");
+   directionsService.route(
+  {
+    //recibimos las propiedades necesarias para que pueda trazar la ruta
+     origin: inputOrigen,
+     destination: inputDestino,
+     travelMode: 'DRIVING'
+   },
+   (response, status) => {
+     console.log("respuesta: ",response,"\n status: ",status);
+     if (status === 'OK') {
+       directionsDisplay.setDirections(response);
+     } /*else {
+       window.alert('Directions request failed due to ' + status);
+     }*///CORREGIR ESTO, EL COMENTARIO ES TEMPORAL
+   });
+ }
